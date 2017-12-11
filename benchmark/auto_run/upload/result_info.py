50770a706ab3004eac050852739270052196a947
Toby Boyd <tobyboyd@google.com>
2017-12-11 15:17:44 -0800

    Results turned into JSON and added unit test

--- benchmark/auto_run/upload/result_info.py
+++ benchmark/auto_run/upload/result_info.py
@@ -1,44 +1,70 @@
 """Structures for a vareity of different test results."""
 
 
-class Result(object):
-  """Information about the results of the test.
+def build_test_result(test_id,
+                      result,
+                      result_type='total_time',
+                      result_units='ms',
+                      test_harness='unknown',
+                      test_environment='unknown'):
+  """Core information about the results of the test.
 
   Args:
       test_id (str): Id when combined with test_source should represent a unique
-      test that maybe run on multiple system types, e.g. P100 or K80.
-      test_name (str): Simplified test name that does not have to be unique and
-        may
-      be useful for display purposes.
+        test that maybe run on multiple system types, e.g. P100 or K80.
       result (float): Float value representing the result of the test.
       result_type (str): Type of result, total_time, exps_per_sec,
-        oom_batch_size, or
-      global_step_per_sec.
-      test_source (str, optional): Test collection, e.g. tf_cnn_benchmarks,
-        keras_benchmarks,
-      model_garden_convergence, or caffe2_bench.
-      result_sounce (str, optional): Source of the test, e.g. DGX-cluster,
-        keras_test_cluster,
-      or manual
+        oom_batch_size, or global_step_per_sec.  Defaults to total_time.
+      result_units (str, optional): Unitest of the results, defaults to ms.
+      test_harness (str, optional): Test collection, e.g. tf_cnn_benchmarks,
+        keras_benchmarks, model_garden_convergence, or caffe2_bench.
+      test_environment (str, optional): Location test was run.
+
+  Returns:
+    Tuple with test_result and result in results array.
+  """
+  test_result = {}
+  test_result['test_id'] = unicode(test_id)
+  test_result['test_harness'] = unicode(test_harness)
+  test_result['test_environment'] = unicode(test_environment)
+
+  results = []
+  result = build_result_info(results, result, result_type, result_units)
+
+  return test_result, results
+
+
+def build_result_info(results,
+                      result,
+                      result_type='total_time',
+                      result_units='ms'):
+  """Appends result dict to end of results array.
+
+  Args:
+    results (str): Array to add result dict into.
+    result (float): Float value representing the result of the test.
+    result_type (str): Type of result, total_time, exps_per_sec,
+      oom_batch_size, or global_step_per_sec.  Defaults to total_time.
+    result_units (str, optional): Unitest of the results, defaults to ms.
+
+  Returns:
+    results appended with new result dict.
 
   """
+  result_entry = {}
+  result_entry['result'] = result
+  result_entry['result_type'] = result_type
+  result_entry['result_units'] = result_units
 
-  def __init__(self,
-               test_id,
-               test_name,
-               result,
-               result_type='total_time',
-               test_source='tf_cnn_bench',
-               result_source='unknown'):
-    self.test_id = unicode(test_id)
-    self.test_name = unicode(test_name)
-    self.result = result
-    self.result_type = unicode(result_type)
-    self.test_source = unicode(test_source)
-    self.result_source = unicode(result_source)
+  results.append(result_entry)
+  return results
 
 
-class SystemInfo(object):
+def build_system_info(platform=None,
+                      platform_type=None,
+                      accel_type=None,
+                      cpu_cores=None,
+                      cpu_type=None):
   """Information about the system the test was executed on.
 
   Args:
@@ -48,48 +74,47 @@ class SystemInfo(object):
       cpu_cores (str, optional): Number of physical cpu cores.
       cpu_type (str, optional): Type of cpu.
 
+  Returns:
+    `dict` with system info.
+
   """
-
-  def __init__(self,
-               platform=None,
-               platform_type=None,
-               accel_type=None,
-               cpu_cores=None,
-               cpu_type=None):
-
-    if platform:
-      self.platform = unicode(platform)
-    if platform_type:
-      self.platform_type = unicode(platform_type)
-    if accel_type:
-      self.accel_type = unicode(accel_type)
-    if cpu_cores:
-      self.cpu_cores = cpu_cores
-    if cpu_type:
-      self.cpu_type = unicode(cpu_type)
+  system_info = {}
+  if platform:
+    system_info['platform'] = unicode(platform)
+  if platform_type:
+    system_info['platform_type'] = unicode(platform_type)
+  if accel_type:
+    system_info['accel_type'] = unicode(accel_type)
+  if cpu_cores:
+    system_info['cpu_cores'] = cpu_cores
+  if cpu_type:
+    system_info['cpu_type'] = unicode(cpu_type)
+  return system_info
 
 
-class TestInfo(object):
+def build_test_info(framework='tensorflow',
+                    batch_size=None,
+                    model=None,
+                    accel_cnt=None):
+  """Initialize TestInfo object.
 
-  def __init__(self,
-               framework='tensorflow',
-               batch_size=None,
-               model=None,
-               accel_cnt=None):
-    """Initialize TestInfo object.
-
-    Args:
-      framework (str, optional): Framework being tested, e.g. tesnsorflow,
+  Args:
+    framework (str, optional): Framework being tested, e.g. tesnsorflow,
       mxnet, or caffe2.  Defaults to tensorflow.
-      batch_size (int, optional): Total batch size.
-      model: Model being tested.
-      accel_cnt (int, optional): Number of accelerators bieng utilized.
-    """
-    if framework:
-      self.framework = framework
-    if batch_size:
-      self.batch_size = batch_size
-    if model:
-      self.model = model
-    if accel_cnt:
-      self.accel_cnt = accel_cnt
+    batch_size (int, optional): Total batch size.
+    model: Model being tested.
+    accel_cnt (int, optional): Number of accelerators bieng utilized.
+
+  Returns:
+    `dict` with test info.
+  """
+  test_info = {}
+  if framework:
+    test_info['framework'] = framework
+  if batch_size:
+    test_info['batch_size'] = batch_size
+  if model:
+    test_info['model'] = model
+  if accel_cnt:
+    test_info['accel_cnt'] = accel_cnt
+  return test_info
--- benchmark/auto_run/upload/result_upload.py
+++ benchmark/auto_run/upload/result_upload.py
@@ -3,76 +3,103 @@ import copy
 from datetime import datetime
 import json
 import os
+import pwd
+import uuid
 
 import pytz
-import result_info
 
 import google.auth
 from google.cloud import bigquery
 from google.cloud.bigquery.dbapi import connect
 
 
-def upload_result(result,
+def upload_result(test_result,
+                  result_info,
                   project,
-                  dataset,
-                  table,
+                  dataset='benchmark_results_dev',
+                  table='result',
                   test_info=None,
                   system_info=None,
-                  extras=None,
-                  debug_level=0):
+                  extras=None):
   """Upload test result.
 
   Note: BigQuery maps unicode() to STRING for python2.  If str is used that is
   mapped to BYTE.
 
   Args:
-    result: Result of the test, strongly suggest use Result.
-    project: Project where BigQuery table is located
+    test_result: `dict` with core info.  Use `result_info.build_test_result`.
+    result_info: `dict` with result info.  Use `result_info.build_test_result`.
+    project: Project where BigQuery dataset is located.
     dataset: BigQuery dataset to use.
     table: BigQuery table to insert into.
-    test_info: Additional test info, suggested use or extend TestInfo.
-    system_info: Extra system info, suggested use or extend SystemInfo.
-    extras: Dictionary of values that will be serialized to JSON.
-    debug_level: Set to 1 for debug info.
+    test_info: `dict` of test info. Use `result_info.build_test_info`.
+    system_info: `dict` of system info. Use `result_info.build_system_info`.
+    extras: `dict` of values that will be serialized to JSON.
   """
 
   # Project is disgarded in favor of what the user passes in.
   credentials, _ = google.auth.default()
 
-  row = copy.copy(result)
-  # The user is set to the email address of the service account.  If that is not
-  # found, then the logged in user is used as a last best guess.
-  if not hasattr(row, 'user'):
-    if hasattr(credentials, 'service_account_email'):
-      row.user = credentials.service_account_email
-    else:
-      row.user = unicode(os.getlogin())
-
-  # gpylint warning suggests using a different lib that does not look helpful.
-  # pylint: disable=W6421
-  setattr(row, 'timestamp', datetime.utcnow().replace(tzinfo=pytz.utc))
-
-  # Convert extra info into JSON.
-  system_info_json = json.dumps(vars(system_info) if system_info else None)
-  test_info_json = json.dumps(vars(test_info) if test_info else None)
-  extras_json = json.dumps(extras)
-  # BigQuery expects unicode object and maps that to datatype.STRING.
-  setattr(row, 'system_info', unicode(system_info_json))
-  setattr(row, 'test_info', unicode(test_info_json))
-  setattr(row, 'extras', unicode(extras_json))
+  row = _build_row(credentials, test_result, result_info, test_info,
+                   system_info, extras)
 
   client = bigquery.Client(project=project, credentials=credentials)
   conn = connect(client=client)
   cursor = conn.cursor()
-  sql = """INSERT into {}.{} (test_id, test_name,
-             test_source, result_source, result, result_type, user,
-             timestamp, system_info, test_info, extras)
-             VALUES (@test_id, @test_name, @test_source, @result_source,
-             @result, @result_type, @user, @timestamp, @system_info, @test_info,
-             @extras)""".format(dataset, table)
+  sql = """INSERT into {}.{} (result_id, test_id, test_harness,
+           test_environment, result_info, user, timestamp, system_info,
+           test_info, extras)
+             VALUES
+           (@result_id, @test_id, @test_harness, @test_environment,
+           @result_info, @user, @timestamp, @system_info, @test_info, @extras)
+           """.format(dataset, table)
 
-  cursor.execute(sql, parameters=vars(row))
+  cursor.execute(sql, parameters=row)
   conn.commit()
   # Cursor and connection closes on their own as well.
   cursor.close()
   conn.close()
+
+
+def _build_row(credentials,
+               test_result,
+               result_info,
+               test_info=None,
+               system_info=None,
+               extras=None):
+  """Builds row to be inserted into BigQuery.
+
+  Note: BigQuery maps unicode() to STRING for python2.  If str is used that is
+  mapped to BYTE.
+
+  Args:
+    credentials: Result of the test, strongly suggest use Result.
+    test_result: `dict` with core info.  Use `result_info.build_test_result`.
+    result_info: `dict` with result info.  Use `result_info.build_test_result`.
+    test_info: `dict` of test info. Use `result_info.build_test_info`.
+    system_info: `dict` of system info. Use `result_info.build_system_info`.
+    extras: `dict` of values that will be serialized to JSON.
+
+  Returns:
+    `dict` to be inserted into BigQuery.
+  """
+  row = copy.copy(test_result)
+  row['result_id'] = unicode(uuid.uuid4())
+  # The user is set to the email address of the service account.  If that is not
+  # found, then the logged in user is used as a last best guess.
+  if hasattr(credentials, 'service_account_email'):
+    row['user'] = credentials.service_account_email
+  else:
+    row['user'] = unicode(pwd.getpwuid(os.getuid())[0])
+
+  # gpylint warning suggests using a different lib that does not look helpful.
+  # pylint: disable=W6421
+  row['timestamp'] = datetime.utcnow().replace(tzinfo=pytz.utc)
+
+  # BigQuery expects unicode object and maps that to datatype.STRING.
+  row['result_info'] = unicode(json.dumps(result_info))
+  row['system_info'] = unicode(json.dumps(system_info if system_info else None))
+  row['test_info'] = unicode(json.dumps(test_info) if test_info else None)
+  row['extras'] = unicode(json.dumps(extras))
+
+  return row
